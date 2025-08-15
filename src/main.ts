e4b28cbbfd9f29073ea47529badc39513597c65a
chi1180 <cwchi1@outlook.jp>
2025-08-16 07:52:59 +0900

    no video available handl

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { YouTubeUtilities } from "./youtube";
 import { EmailUtilities } from "./email";
+import { YouTubeUtilities } from "./youtube";
 
 function main() {
   const _YouTubeUtilities = new YouTubeUtilities();
@@ -7,12 +7,17 @@ function main() {
 
   const formattedData = _YouTubeUtilities.run();
 
-  const mailInfo = {
-    subject: "Subscribed YouTube channels' new videos reminder",
-    htmlPath: "template.html",
-    mailData: formattedData,
-  };
-  _EmailUtilities.sendMail(mailInfo.subject, mailInfo.htmlPath, mailInfo.mailData);
+  const is_newVideosAvailable = formattedData.length > 0;
+  if (is_newVideosAvailable) {
+    const mailInfo = {
+      subject: "Subscribed YouTube channels' new videos reminder",
+      htmlPath: "template.html",
+      mailData: formattedData,
+    };
+    _EmailUtilities.sendMail(mailInfo.subject, mailInfo.htmlPath, mailInfo.mailData);
+  } else {
+    console.log("[--INFO--] There are no new videos from your subscribing YouTube channels...");
+  }
 }
 
 export { main };
